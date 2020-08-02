d733f6f16528b771c5adb210c7ece92c5ceb5dff
Mrigankx <mrigankagoswami03@gmail.com>
2020-08-02 19:14:23 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
-import pygame, sys
-from game import Game
-def main():
-    game = Game()
-    game.run()
-    quit()
-if __name__ == "__main__":
-    main()
+# import pygame, sys
+# from game import Game
+# def main():
+#     game = Game()
+#     game.run()
+#     quit()
+# if __name__ == "__main__":
+#     main()
+print("Hello world")
